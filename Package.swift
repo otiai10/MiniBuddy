78aff3cd6b69e3e94318611b0f48f9024f21be2c
Hiromu OCHIAI <otiai10@gmail.com>
2020-03-13 14:26:08 +0200

    Setup dependencies

--- Package.swift
+++ Package.swift
@@ -5,18 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "MiniBuddy",
+    products: [
+        .executable(name: "minibuddy", targets: ["minibuddy"]),
+        // .library(name: "MiniBuddyFramework", targets: ["Framework"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Carthage/Commandant.git", .upToNextMinor(from: "0.17.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MiniBuddy",
-            dependencies: []),
+            name: "minibuddy",
+            dependencies: [
+                "Commandant",
+                // "MiniBuddyFramework",
+            ]
+        ),
+        // .target(
+        //     name: "MiniBuddyFramework",
+        //     dependencies: []
+        // ),
         .testTarget(
             name: "MiniBuddyTests",
-            dependencies: ["MiniBuddy"]),
+            dependencies: ["minibuddy"]
+        )
     ]
 )
